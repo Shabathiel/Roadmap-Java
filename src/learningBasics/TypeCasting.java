cc8d3dd43f9bd334a9e4a535480408714c2b83bb
Shabathiel <schavezr5@miumg.edu.gt>
2026-03-18 23:08:50 -0600

    Type Casting

--- src/learningBasics/TypeCasting.java
+++ src/learningBasics/TypeCasting.java
@@ -3,8 +3,50 @@ package learningBasics;
 public class TypeCasting {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//Widening Conversion
+	    int num = 50;
 
+	    System.out.println("The integer value: " + num);
+
+	    double data = num;
+
+	    System.out.println("The double value: " + data);
+	    
+	    //Narrowing type conversion
+	    double num2 = 50.55;
+
+	    System.out.println("The double value: " + num);
+
+
+	    int data2 = (int)num;
+
+	    System.out.println("The integer value: " + data);
+	    
+	    //Int to String
+	    
+	    int num3 = 50;
+
+	    System.out.println("The integer value is: " + num);
+
+
+	    String data3 = String.valueOf(num);
+
+	    System.out.println("The string value is: " + data);
+
+	    //String to int
+	    
+	    String data4 = "50";
+
+	    System.out.println("The string value is: " + data);
+
+
+	    int num4 = Integer.parseInt(data4);
+
+	    System.out.println("The integer value is: " + num);
+
+	    //Orden para convertir (mas pequeño al mas grande)
+	    //byte -> short -> char -> int -> long -> float -> double  
 	}
 
 }
