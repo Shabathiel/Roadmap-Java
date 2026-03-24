4f5d44a67f753f1521891bf35b209812e4fa0942
Shabathiel <schavezr5@miumg.edu.gt>
2026-03-23 23:26:25 -0600

    Correción a error de variables

--- src/learningBasics/TypeCasting.java
+++ src/learningBasics/TypeCasting.java
@@ -16,34 +16,34 @@ public class TypeCasting {
 	    //Narrowing type conversion
 	    double num2 = 50.55;
 
-	    System.out.println("The double value: " + num);
+	    System.out.println("The double value: " + num2);
 
 
 	    int data2 = (int)num2;
 
-	    System.out.println("The integer value: " + data);
+	    System.out.println("The integer value: " + data2);
 	    
 	    //Int to String
 	    
 	    int num3 = 50;
 
-	    System.out.println("The integer value is: " + num);
+	    System.out.println("The integer value is: " + num3);
 
 
 	    String data3 = String.valueOf(num3);
 
-	    System.out.println("The string value is: " + data);
+	    System.out.println("The string value is: " + data3);
 
 	    //String to int
 	    
 	    String data4 = "50";
 
-	    System.out.println("The string value is: " + data);
+	    System.out.println("The string value is: " + data4);
 
 
 	    int num4 = Integer.parseInt(data4);
 
-	    System.out.println("The integer value is: " + num);
+	    System.out.println("The integer value is: " + num4);
 
 	    //Orden para convertir (mas pequeño al mas grande)
 	    //byte -> short -> char -> int -> long -> float -> double  
